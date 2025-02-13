221fb91186e74024f2c7fce3bdb4b435cf876e7d
Kavisha <11203643@students.cs.ubc.ca>
2025-02-13 15:22:33 -0800

    added specs for methods for savings

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -8,6 +8,7 @@ public class Account {
     private int balance;
     private ArrayList<Transaction> listOfEarnings;
     private ArrayList<Transaction> listOfExpenses;
+    private int savings;
 
     // constructs an account with balance set to 0 and empty list of earnings and
     // expenses
@@ -15,6 +16,7 @@ public class Account {
         balance = 0;
         listOfEarnings = new ArrayList<>();
         listOfExpenses = new ArrayList<>();
+        savings = 0;
     }
 
     // returns the account balance
@@ -31,5 +33,31 @@ public class Account {
     public ArrayList<Transaction> getListOfExpenses() {
         return null;
     }
+
+    // MODIFIES: this
+    // EFFECTS: adds an expense, withdraws amount from balance, and adds it to the list of Expenses
+    public void addExpense(){
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds an earning and adds it to the list of Earnings, adds amount to balance
+    public void addEarning(){
+        
+    }
+
+    // REQUIRES: savings + amount <= target
+    // MODIFIES: this
+    // EFFECTS: adds amount to savings
+    public void saveAmount(){
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets a target for savings
+    public void setSavingTarget(){
+        
+    }
+
 
 }
