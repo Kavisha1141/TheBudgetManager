fe172750caa5b8658bc7936b6792b2efcd60b542
Kavisha <11203643@students.cs.ubc.ca>
2025-02-13 13:34:40 -0800

    declared fields, stubs for getters and setters of Earnings class

--- src/main/model/Earnings.java
+++ src/main/model/Earnings.java
@@ -1,5 +1,60 @@
 package model;
 
 public class Earnings {
+    private int amount;
+    private int month;
+    private int day;
+    private int year;
+    private String title;
+
+    // constructs an Earning of amount amt, date consisting of day,month,year and title
+    public Earnings(){
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the amount to amt
+    public void setAmount(){
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the date
+    public void setDate(int day, int month, int year){
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the title
+    public void setTitle(String title){
+
+    }
+
+    // returns the day
+    public int getDay() {
+        return -1;
+    }
+
+    // returns the month
+    public int getMonth() {
+        return -1;
+    }
+
+    // returns the year
+    public int getYear() {
+        return -1;
+    }
+
+    // returns the amount for the Earning
+    public int getAmount() {
+        return -1;
+    }
+
+    // returns the title for the Earning
+    public String getTitle() {
+        return "";
+    }
+
+
 
 }
