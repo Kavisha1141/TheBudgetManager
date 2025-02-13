a32f5d04a2a94fd74ab6bfbf7ab1de076ceef738
Kavisha <11203643@students.cs.ubc.ca>
2025-02-13 00:02:50 -0800

    checkstyle

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -9,24 +9,24 @@ public class Account {
     private ArrayList<Expense> listOfExpenses;
 
     // constructs an account with balance set to 0 and empty list of earnings and expenses
-    public Account(){
+    public Account() {
         balance = 0;
         listOfEarnings = new ArrayList<>();
         listOfExpenses = new ArrayList<>();
     }
 
     // returns the account balance
-    public int getBalance(){
+    public int getBalance() {
         return -1;
     }
 
     // returns the list of earnings
-    public ArrayList<Earnings> getListOfEarnings(){
+    public ArrayList<Earnings> getListOfEarnings() {
         return null;
     }
 
     // returns the list of expenses
-    public ArrayList<Expense> getListOfExpenses(){
+    public ArrayList<Expense> getListOfExpenses() {
         return null;
     } 
 
