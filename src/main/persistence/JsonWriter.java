486acb9e342b65dfe8e75c6e91d9e0148ca7fa1e
Kavisha <11203643@students.cs.ubc.ca>
2025-02-27 19:18:11 -0800

    added specs and stubs for methods of Writer class in persistence package

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,45 @@
 package persistence;
 
-public class JsonWriter {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import model.Account;
+import org.json.JSONObject;
 
+// Represents a writer that writes JSON representation of Account to file
+// CODE SOURCE: JsonSerializationDemo file: https://github.com/stleary/JSON-java
+public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+       
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of Account to file
+    public void write(Account acc) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        
+    }
 }
