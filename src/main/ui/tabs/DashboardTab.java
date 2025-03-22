c144f22a03603190168549531b05364cb8e29b1d
Kavisha <11203643@students.cs.ubc.ca>
2025-03-21 18:06:46 -0700

    specified and implemented printInfo method in dashboard

--- src/main/ui/tabs/DashboardTab.java
+++ src/main/ui/tabs/DashboardTab.java
@@ -1,5 +1,7 @@
 package ui.tabs;
 
+import java.util.List;
+import java.util.ArrayList;
 import javax.swing.JLabel;
 import ui.BudgetManagerUI;
 
@@ -10,12 +12,18 @@ public class DashboardTab extends Tab {
 
     private String greeting;
     private JLabel greet; 
+    private JLabel accountBalanceLabel;
+    private JLabel totalEarningsLabel;
+    private JLabel totalExpensesLabel;
+    private JLabel totalSavingsLabel;
+    private JLabel savingsTargetLabel;
 
     //EFFECTS: constructs a home tab for console with buttons and a greeting
     public DashboardTab(BudgetManagerUI controller) {
         super(controller);
         setLayout(new GridLayout(3, 1));
         placeGreeting();
+        printInfo(setAccountInfo());
     }
 
     // initializes the greeting
@@ -23,6 +31,7 @@ public class DashboardTab extends Tab {
         greeting = "Welcome, "+ getController().getAccount().getName()+ "!";
     }
 
+    //MODIFIES: this
     //EFFECTS: creates greeting at top of console
     private void placeGreeting() {
         setGreeting();
@@ -30,5 +39,38 @@ public class DashboardTab extends Tab {
         greet.setSize(WIDTH, HEIGHT / 3);
         this.add(greet);
     }
+
+    //MODIFIES: this
+    //EFFECTS: sets Account info 
+    private List<JLabel> setAccountInfo() {
+        Integer accountBalance = this.getController().getAccount().getBalance();
+        Integer totalEarnings = this.getController().getAccount().getTotalEarnings();
+        Integer totalExpenses = this.getController().getAccount().getTotalExpenses();
+        Integer totalSavings = this.getController().getAccount().getSavings();
+        Integer savingsTarget = this.getController().getAccount().getSavingsTarget();
+        accountBalanceLabel = new JLabel("$" + accountBalance.toString());
+        totalEarningsLabel = new JLabel("$" + totalEarnings.toString());
+        totalExpensesLabel = new JLabel("$" + totalExpenses.toString());
+        totalSavingsLabel = new JLabel("$" + totalSavings.toString());
+        savingsTargetLabel = new JLabel("$" + savingsTarget.toString());
+        List<JLabel> listOfInfo = new ArrayList<>();
+        listOfInfo.add(accountBalanceLabel);
+        listOfInfo.add(totalEarningsLabel);
+        listOfInfo.add(totalExpensesLabel);
+        listOfInfo.add(totalSavingsLabel);
+        listOfInfo.add(savingsTargetLabel);
+        return listOfInfo;
+    }
+
+    //MODIFIES: this
+    //EFFECTS: places all info
+    private void printInfo(List<JLabel> list) {
+        for (JLabel label: list) {
+            label.setSize(WIDTH, HEIGHT / 3);
+            this.add(greet);
+        }
+    }
+
+
 
 }
