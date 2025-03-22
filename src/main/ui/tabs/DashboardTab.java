44ba22140885711dcd7170ecac945c0bd96d24f1
Kavisha <11203643@students.cs.ubc.ca>
2025-03-22 15:18:37 -0700

    added progress bar

--- src/main/ui/tabs/DashboardTab.java
+++ src/main/ui/tabs/DashboardTab.java
@@ -16,22 +16,24 @@ public class DashboardTab extends Tab {
     private JLabel totalEarningsLabel;
     private JLabel totalExpensesLabel;
     private JLabel totalSavingsLabel;
-    private JLabel savingsTargetLabel;
     private List<JLabel> listOfInfo;
     private  JPanel infoPanel;
     private JPanel mainBlock;
+    private JProgressBar progressBar;
 
     //EFFECTS: constructs a home tab for console with buttons and a greeting
     public DashboardTab(BudgetManagerUI controller) {
         super(controller);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 0));
-        mainBlock = new JPanel(new GridLayout(4, 1, 0, 15));
+        setBorder(BorderFactory.createEmptyBorder(5, 15, 0, 0));
+        mainBlock = new JPanel(new GridLayout(3, 1, 0, 5));
         mainBlock.setAlignmentX(LEFT_ALIGNMENT);
         placeGreeting();
         printInfo(setAccountInfo());
-        setUpButtons();
+        setUpButtons(); 
+        
         this.add(mainBlock);
+        addProgressBar();
     }
 
     // initializes the greeting
@@ -59,7 +61,6 @@ public class DashboardTab extends Tab {
         totalEarningsLabel = new JLabel(); 
         totalExpensesLabel = new JLabel(); 
         totalSavingsLabel = new JLabel(); 
-        savingsTargetLabel = new JLabel();
 
         updateAccountInfo(); 
         
@@ -68,7 +69,6 @@ public class DashboardTab extends Tab {
         listOfInfo.add(totalEarningsLabel);
         listOfInfo.add(totalExpensesLabel);
         listOfInfo.add(totalSavingsLabel);
-        listOfInfo.add(savingsTargetLabel);
         return listOfInfo;
     }
 
@@ -95,39 +95,51 @@ public class DashboardTab extends Tab {
         Integer totalEarnings = this.getController().getAccount().getTotalEarnings();
         Integer totalExpenses = this.getController().getAccount().getTotalExpenses();
         Integer totalSavings = this.getController().getAccount().getSavings();
-        Integer savingsTarget = this.getController().getAccount().getSavingsTarget();
     
         accountBalanceLabel.setText("Account Balance: $" + accountBalance);
         totalEarningsLabel.setText("Total Earnings: $" + totalEarnings);
         totalExpensesLabel.setText("Total Expenses: $" + totalExpenses);
         totalSavingsLabel.setText("Total Savings: $" + totalSavings);
-        savingsTargetLabel.setText("Savings Target: $" + savingsTarget);
-    
-        this.revalidate(); // Refresh layout
+
+        this.revalidate(); 
         this.repaint();
+    }
+
+    //EFFECTS: updates progress bar
+    private void updateProgressBar() {
+        if (this.getController().getAccount().getTotalEarnings() != 0) {
+            int progressValue = (int) ((double) this.getController().getAccount().getSavings() / this.getController().getAccount().getTotalEarnings() * 100); 
+            progressBar.setValue(progressValue); 
+        }
+        progressBar.setStringPainted(true);
+        mainBlock.revalidate();
+        mainBlock.repaint();
+        this.revalidate();
+        this.repaint();
+    }
+
+    //EFFECTS: updates entire dashboard
+    private void updateDashboard() {
+        updateAccountInfo();
+        updateProgressBar();
     }
 
     //EFFECTS: sets up savings, update and save to file and quit app button
     private void setUpButtons() {
-        
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 2));
         buttonPanel.add(setUpUpdateButton());
         buttonPanel.add(setUpSaveButton());
         buttonPanel.add(setUpAddToSavingsButton());
-
-        buttonPanel.setPreferredSize(new Dimension(100, 30));
-       
-        this.add(Box.createRigidArea(new Dimension(0, 15)));
+        buttonPanel.setPreferredSize(new Dimension(100, 10));
         mainBlock.add(buttonPanel);
-
     }
 
 
     //EFFECTS: sets up update button
     private JButton setUpUpdateButton() {
         JButton update = new JButton("Update info");
-        update.addActionListener(e -> updateAccountInfo());
+        update.addActionListener(e -> updateDashboard());
         return update;
     }
 
@@ -159,6 +171,59 @@ public class DashboardTab extends Tab {
 
     //EFFECTS: adds to savings of account 
     private void addToSavings() {
-
+        JButton save = new JButton("Save");
+        JDialog savingsDialog = new JDialog((Frame) null, "Save Amount", false);
+        JTextField amountField = new JTextField();
+        savingsDialog.add(new JLabel("Amount:"));
+        savingsDialog.add(amountField);
+        savingsDialog.add(save);
+        savingsDialog.setLayout(new GridLayout(1,1));
+        savingsDialog.setSize(250, 90);
+        savingsDialog.setLocationRelativeTo(this); 
+        savingsDialog.setVisible(true);
+        save.addActionListener(e -> addSavings(amountField)); 
     }
+
+    // adds progress bar 
+    private void addProgressBar() {
+        JLabel progress = new JLabel("Your savings with respect to total earnings: ");
+        progress.setPreferredSize(new Dimension(50, 10));
+        progress.setFont(new Font("Arial", Font.PLAIN, 12));
+        progressBar = new JProgressBar();
+        progressBar.setMinimum(0); 
+        progressBar.setMaximum(100); 
+        progressBar.setValue(0); 
+        if (this.getController().getAccount().getTotalEarnings() != 0) {
+            int progressValue = (int) ((double) this.getController().getAccount().getSavings() / this.getController().getAccount().getTotalEarnings() * 100);
+            progressBar.setValue(progressValue); 
+        }
+        progressBar.setStringPainted(true);
+        this.add(progress);
+        this.add(progressBar);
+    }
+
+    //EFFECTS: show not enough balance popup
+    public void notEnoughBalancePopUp() {
+        JLabel notEnoughBalance = new JLabel("Not enough balance", SwingConstants.CENTER);
+        notEnoughBalance.setFont(new Font("Arial", Font.PLAIN, 12));
+        notEnoughBalance.setAlignmentX(CENTER_ALIGNMENT);
+        JDialog newDialog = new JDialog((Frame) null, "Add Savings", false);
+        newDialog.setLayout(new GridLayout(1,1));
+        newDialog.add(notEnoughBalance);
+        newDialog.setSize(250, 150);
+        newDialog.setLocationRelativeTo(this); 
+        newDialog.setVisible(true);
+    }
+
+    //EFFECTS: Adds amount to savings, shows popup if not enough amount
+    public void addSavings(JTextField amountField) {
+        int amount = Integer.parseInt(amountField.getText());
+        if (this.getController().getAccount().getBalance() - amount < 0) {
+            notEnoughBalancePopUp();
+            return;
+        }
+        this.getController().getAccount().saveAmount(amount);
+    }
+
+
 }
